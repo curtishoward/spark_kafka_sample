94ff37d4eff90e02fdf194c8c97ef3b94a50be91
Curtis Howard <curtis.james.howard@gmail.com>
2020-05-20 08:42:57 -0400

    Update SparkKafka.scala

--- src/main/scala/com/cloudera/fce/curtis/spark_kafka_sample/SparkKafka.scala
+++ src/main/scala/com/cloudera/fce/curtis/spark_kafka_sample/SparkKafka.scala
@@ -25,28 +25,11 @@ object SparkKafka {
 
     val dstream         = KafkaUtils.createDirectStream[String, String](ssc, LocationStrategies.PreferConsistent, 
                                      ConsumerStrategies.Subscribe[String, String](topicsSet, kafkaParams))
-//    val windowedStream  = dstream.window(Seconds(60))
 
-//    windowedStream.foreachRDD { rdd =>
     dstream.foreachRDD { rdd =>
        import spark.implicits._
-       
-       // val dataFrame = rdd.map(rec => (rec.value.split(",")(0).toLong,rec.value.split(",")(1).toInt))
-//			  .toDF("measurement_time","number_of_vehicles")
        val dataFrame = rdd.map(rec => (Instant.now.getEpochSecond, rec.value)).toDF("timestamp", "message_string")
-//			  .toDF("measurement_time","number_of_vehicles")
        dataFrame.show()
-//       dataFrame.registerTempTable("traffic")
-
-//       val resultsDataFrame = spark.sql("""SELECT UNIX_TIMESTAMP() * 1000 as_of_time, 
-//	                                            ROUND(AVG(number_of_vehicles), 2) avg_num_veh,
-//						    MIN(number_of_vehicles) min_num_veh, 
-//						    MAX(number_of_vehicles) max_num_veh,
-//		                                    MIN(measurement_time) first_meas_time, 
-//						    MAX(measurement_time) last_meas_time 
-//						FROM traffic""")
- //      resultsDataFrame.show()
-
     }
 
     ssc.start()
